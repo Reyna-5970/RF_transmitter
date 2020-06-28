84fdee096b87e70f1f8661edabdd2711cf2d6929
Reyna-5970 <55515949+Reyna-5970@users.noreply.github.com>
2020-06-27 17:04:41 -0700

    Documented wiring. Created directory

--- Code/Transmitter/Transmitter.ino
+++ Code/Transmitter/Transmitter.ino
@@ -2,6 +2,16 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
+// wiring -->
+// VCC+ to 3.3V
+// GND to GND
+// CSN to digital-pwm (8)
+// CE to digital-pwm (7)
+// MOSI to 51
+// SCK to 52
+// MISO to 50
+// IRQ to 48 ??
+
 RF24 radio(7, 8); // CE, CSN
 
 const byte address[6] = "00001";
