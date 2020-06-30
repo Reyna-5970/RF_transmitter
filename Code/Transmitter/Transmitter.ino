2c3c584050c63ddf2261e7d1d99a6d9a634951b0
Reyna-5970 <55515949+Reyna-5970@users.noreply.github.com>
2020-06-30 13:05:40 -0700

    Fixed library issue
    https://www.arduino.cc/en/guide/libraries

--- Code/Transmitter/Transmitter.ino
+++ Code/Transmitter/Transmitter.ino
@@ -1,6 +1,6 @@
 #include <SPI.h>
-#include </Users/reyna/Desktop/RF24/nRF24L01.h>
-#include </Users/reyna/Desktop/RF24/RF24.h>
+#include <nRF24L01.h>
+#include <RF24.h>
 
 // wiring -->
 // VCC+ to 3.3V
@@ -10,21 +10,26 @@
 // MOSI to 51
 // SCK to 52
 // MISO to 50
-// IRQ to 48 ??
+// IRQ to 48 ?? Check SPI.h
 
 RF24 radio(7, 8); // CE, CSN
 
 const byte address[6] = "00001";
+
+int ledStatus = 0; //0 is off, 1 is on
 
 void setup() {
   radio.begin();
   radio.openWritingPipe(address);
   radio.setPALevel(RF24_PA_MIN);
   radio.stopListening();
+  currentStatus();
 }
 
 void loop() {
-  const char text[] = "Hello World";
-  radio.write(&text, sizeof(text));
-  delay(1000);
+}
+
+int currentStatus() {
+  ledStatus = 1;
+  return ledStatus;
 }
